027f0731c1e7508b125af15098b353a608aed031
Kirill Marenov <kmarenov@gmail.com>
2019-07-27 14:58:54 +0300

    Error Handling: Getting a non-existing Task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task } from 'dist/tasks/task.model';
 import { TaskStatus } from './task.model';
 import * as uuid from 'uuid/v1';
@@ -32,7 +32,13 @@ export class TasksService {
     }
 
     getTaskById(id: string): Task {
-        return this.tasks.find(task => task.id === id);
+        const found = this.tasks.find(task => task.id === id);
+
+        if (!found) {
+            throw new NotFoundException(`Task with ID "${id}" not foundgit`);
+        }
+
+        return found;
     }
 
     deleteTask(id: string): void {
