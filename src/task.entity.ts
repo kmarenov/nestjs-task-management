ab10ecadc66aa8a6f08e185359a2848f98509637
Kirill Marenov <kmarenov@gmail.com>
2019-08-08 10:09:18 +0300

    Creating a Task Entity

---
+++ src/task.entity.ts
@@ -0,0 +1,17 @@
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { TaskStatus } from "./tasks/task.model";
+
+@Entity()
+export class Task extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    title: string;
+
+    @Column()
+    description: string;
+
+    @Column()
+    status: TaskStatus;
+}
