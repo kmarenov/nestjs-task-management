c351a7d793f09e15d77259852a78b3fc79f6ff7e
Kirill Marenov <kmarenov@gmail.com>
2019-08-14 10:24:01 +0300

    IMPORTANT: Before Persistence of Getting Tasks

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -14,27 +14,9 @@ export class TasksService {
         private taskRepository: TaskRepository,
     ){}
 
-    // getAllTasks(): Task[] {
-    //     return this.tasks;
-    // }
-
-    // getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
-    //     const { status, search } = filterDto;
-
-    //     let tasks = this.getAllTasks();
-
-    //     if (status) {
-    //         tasks = tasks.filter(task => task.status === status);
-    //     }
-
-    //     if (search) {
-    //         tasks = tasks.filter(task =>
-    //             task.title.includes(search) || task.description.includes(search),
-    //         );
-    //     }
-
-    //     return tasks;
-    // }
+    getTasks(filterDto: GetTasksFilterDto) {
+        //
+    }
 
     async getTaskById(id: number): Promise<Task> {
         const found = await this.taskRepository.findOne(id);
