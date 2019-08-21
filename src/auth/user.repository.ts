0250b648643325bacd35dd24873b5cab8ce76c6c
Kirill Marenov <kmarenov@gmail.com>
2019-08-21 10:52:41 +0300

    Setting up AuthModule, User Entity and UserRepository

---
+++ src/auth/user.repository.ts
@@ -0,0 +1,7 @@
+import { Repository, EntityRepository } from "typeorm";
+import { User } from "./user.entity";
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+
+}
