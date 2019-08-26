09f11b0178195ec1f1ebc31b9b75ffe78de61787
Kirill Marenov <kmarenov@gmail.com>
2019-08-26 10:12:23 +0300

    Validation: AuthCredentialsDto, password strength

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { AuthService } from './auth.service';
 
@@ -9,7 +9,7 @@ export class AuthController {
     ) {}
     
     @Post('/signup')
-    signUp(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    signUp(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise<void> {
         return this.authService.signUp(authCredentialsDto);
     }
 }
