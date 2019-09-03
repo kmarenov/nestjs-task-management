b7f5d80b01fa533f46cc25ebd84df819e10c1056
Kirill Marenov <kmarenov@gmail.com>
2019-09-03 09:40:45 +0300

    Tasks and Users - Database Relation

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -3,9 +3,9 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm'
 export const typeOrmConfig: TypeOrmModuleOptions = {
     type: 'postgres',
     host: 'localhost',
-    port: 5432,
+    port: 5433,
     username: 'postgres',
-    password: 'postgres',
+    password: 'root',
     database: 'taskmanagement',
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
     synchronize: true,
