da34eb0341a05d9949289be8532fe73128c851b6
Kirill Marenov <kmarenov@gmail.com>
2019-09-11 10:04:17 +0300

    Front-end Application Set-up (Development Mode)

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,10 @@ async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
 
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  }
+
   const port = process.env.PORT || serverConfig.port;
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
