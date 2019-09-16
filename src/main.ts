69348dde4acdbab01cc6b1c743cadd5f40c134ae
Kirill Marenov <kmarenov@gmail.com>
2019-09-16 10:17:38 +0300

    Enabling CORS from the S3 Front-end Application

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,9 @@ async function bootstrap() {
 
   if (process.env.NODE_ENV === 'development') {
     app.enableCors();
+  } else {
+    app.enableCors({ origin: serverConfig.origin });
+    logger.log(`Accepting requests from origin "${serverConfig.origin}"`);
   }
 
   const port = process.env.PORT || serverConfig.port;
